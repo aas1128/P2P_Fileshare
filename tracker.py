3e57c099c1e49432aa11d6416e5e508a764e677e
Finn <fss5045@rit.edu>
2025-04-14 13:17:40 -0400

    tracker generates metainfo from file, also added cleanup

--- tracker.py
+++ tracker.py
@@ -1,3 +1,4 @@
+import os
 import time
 import socket
 from threading import *
@@ -8,30 +9,54 @@ tr_port = 9000
 sock.bind(('127.0.0.1', tr_port))
 p_len = 8
 seeders = {}
+peer_timeout = 60
 file_pieces = {}
 dl_peers = []
 dling_peers = []
 wait = 5
 
 
-def generate_metainfo(name, file_len):
+def generate_metainfo(file_path):
+    # get relevant info from base file
+    file = os.path.basename(file_path)
+    file_name, file_ext = os.path.splitext(file)
+    file_len = os.path.getsize(file_path)
+
+    # compute info for metainfo file
     pieces = []
     for i in range(0, file_len, p_len):
-        pieces.append(i)
-    file_pieces[name] = [str(item) for item in pieces]
-    with open(f'{name}.torrent', 'w') as file:
-        file.write(f'{{announce: (\'127.0.0.1\', {tr_port}), info: {{name: {name}, piece_length: {p_len}, pieces: {pieces}, length: {file_len}}}}}')
+        pieces.append(str(i))
+    file_pieces[file] = pieces
+
+    # write to metainfo .torrent file
+    with open(f'{file_name}.torrent', 'w') as f:
+        f.write(f'{{announce: (\'127.0.0.1\', {tr_port}), info: {{name: {file}, piece_length: {p_len}, pieces: {pieces}, length: {file_len}}}}}')
 
 
-def discover_peer():
+def discover_peers():
     while 1:
+        # receive broadcasts from peers
         pkt, sender = sock.recvfrom(100)
+        print(f'Received: {pkt}')
+        # clean up packet data
         port, name, received, = pkt.decode().split('|')
         port = int(port)
         received = received[1:-1].split(', ')
-        seeders[port] = (name, received)
-        print(f'Received: {pkt}')
+        # add to seeders and downloaders
+        curr_time = time.time()
+        seeders[port] = (name, received, curr_time)
         dl_peers.append((port, name, received))
+
+
+def cleanup_seeders():
+    while 1:
+        # every 30s, check for 'dead' peers
+        for port, (name, received, rcv_time) in seeders.items():
+            curr_time = time.time()
+            if (curr_time - rcv_time) > peer_timeout:
+                # if a peer hasnt been heard from in <60>s, remove from seeders
+                seeders.pop(port)
+        time.sleep(30)
 
 
 def match_peers(dl_peer):
@@ -43,10 +68,12 @@ def match_peers(dl_peer):
         print(f'{dl_port} already has entire {dl_name} file')
         dling_peers.remove(dl_peer)
         return
-    for port, (name, received) in seeders.items():
+    for port, (name, received, rcv_time) in seeders.items():
         if (port, name, received) == dl_peer or name != dl_name:
+            # if same peer
             continue
         if set(received) & needed:
+            # if there is an intersection of what the peer has and what dl_peer needs
             print(f'matching {dl_port} with {port}')
             info = f'{port}|{name}|{received}'.encode()
             addr = ('127.0.0.1', dl_port)
@@ -56,16 +83,25 @@ def match_peers(dl_peer):
     
 
 def main():
-    generate_metainfo('spiderman', 64)
-    discover_thread = Thread(target=discover_peer, daemon=True)
+    # generate metainfo file
+    generate_metainfo('spiderman.txt')
+
+    # start thread to discover new peers
+    discover_thread = Thread(target=discover_peers, daemon=True)
     discover_thread.start()
+
+    # start thread to remove 'dead' seeders from list
+    cleanup_thread = Thread(target=cleanup_seeders, daemon=True)
+    cleanup_thread.start()
 
     while 1:
         print(f'Peers Downloading: {dling_peers}')
         if dl_peers:
+            # if there are peers that want to download something, get oldest in list
             current_peer = dl_peers.pop(0)
             if current_peer not in dling_peers:
                 dling_peers.append(current_peer)
+            # start thread to match the current peer to a peer that has what it needs
             match_thread = Thread(target=match_peers, args=(current_peer,), daemon=True)
             match_thread.start()
         time.sleep(wait)
